669f3b4627449309493413924f56bd6e9194da84
origin_master <https://github.com/YudhaDwipriatma/UTS1.git>
2019-04-26 20:47:43 +0700

    selesai

---
+++ Intent/app/src/main/java/id/sch/smktelkom_mlg/www/intent/TargetActivity.java
@@ -0,0 +1,25 @@
+package id.sch.smktelkom_mlg.www.intent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TargetActivity extends AppCompatActivity {
+
+    public TextView txtResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_target);
+        txtResult = findViewById(R.id.textView);
+
+        //get data
+        if (getIntent().getExtras() != null) {
+            String data = getIntent().getExtras().getString(MainActivity.EXTRA_DATA);
+            txtResult.setText(data);
+
+        }
+
+    }
+}
